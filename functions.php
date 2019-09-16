b5b2674707b2105b25882cd397474d8940247a2a
Watheq <watheq.alshowaiter@gmail.com>
2019-09-16 21:15:45 +0300

    done #44 - adding jquery from Wordpress scripts and make it in footer

--- functions.php
+++ functions.php
@@ -11,7 +11,6 @@ function elzero_add_style()
     // if I use RTL bootstrab 
     // wp_enqueue_style('bootstrap-rtl-css', get_template_directory_uri() . '/source/css/bootstrap_rtl.min.css'); 
     wp_enqueue_style('fontawsome-css', get_template_directory_uri() . '/source/css/all.fonts.min.css');
-    
 }
 
 function elzero_add_script()
@@ -21,11 +20,18 @@ function elzero_add_script()
     // wp_enqueue_script('jquery-js', get_template_directory_uri() . '/source/js/jquery.min.js', array(), false, true);
     // wp_enqueue_script('jquery');  // make before bootsape 
     // maybe bootsrap bundle do all stuff. if so I will delete bootstrap.js or just bootstrap is enough
+
+    // remove registration from old jquery 
+    wp_deregister_script('jquery');
+    // add register script in the footer 
+    wp_register_script('jquery', includes_url('/js/jquery/jquery.js'), array(), false, true);
+    // enque jquery in footer 
+    wp_enqueue_script('jquery'); 
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/source/js/bootstrap.min.js', array('jquery'), false, true);
     // wp_enqueue_script('bootstrap-bundle-js', get_template_directory_uri() . '/source/js/bootstrap.bundle.min.js', array('jquery'), false, true); 
     wp_enqueue_script('main-js', get_template_directory_uri() . '/source/js/main.js', array(), false, true);
 }
 
 
-add_action('wp_enqueue_scripts','elzero_add_style'); 
-add_action('wp_enqueue_scripts','elzero_add_script'); 
+add_action('wp_enqueue_scripts', 'elzero_add_style');
+add_action('wp_enqueue_scripts', 'elzero_add_script');
