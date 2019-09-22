d9bb7fb7ef7bedf3d1f9455aa67b533d3cef82cf
Watheq <watheq.alshowaiter@gmail.com>
2019-09-23 00:49:47 +0300

    adding content dynamically + fixing word wrapin content to fit main-post div

--- index.php
+++ index.php
@@ -21,9 +21,18 @@
                         <!--  the_author_posts_link(): the author page of all his posts -->
                         <span class="post-author"><i class="fas fa-user"></i> <?php the_author_posts_link(); ?></span>
                         <span class="post-date"> <i class="fas fa-calendar-alt"></i> <?php the_time('d, m, Y'); ?></span>
-                        <span class="post-comments"> <i class="fas fa-comments"></i> <?php comments_popup_link("No Comments","1 Comment", "% Comments", "comment-url", "Comments Off"); ?></span>
+                        <span class="post-comments"> <i class="fas fa-comments"></i> <?php comments_popup_link("No Comments", "1 Comment", "% Comments", "comment-url", "Comments Off"); ?></span>
+                        <!-- To not mix comment count with contnt if there is no feautered image-->
+                        <br>
+                        <!-- post imsge thunmbnail -->
+                        <!-- 1st arg is for sizes like 'thumbnals' 150x150 and other sizes in worpreess docs -->
+                        <?php the_post_thumbnail("", ["class" => "img-fluid img-thumbnail", "alt" => "place holder image", "title" => "post image"]); ?>
+                        <div class="post-content" id="post-content">
+                            <?php the_content('Continue.. '); ?>
+                        </div>
+                        <!-- <span class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus possimus asperiores fuga dolorum. Rem laboriosam fuga eum et repudiandae similique, obcaecati aliquid harum enim ab voluptatum cupiditate consectetur perferendis qui.</span> -->
                         <hr>
-                        <p class="categories"><i class="fas fa-tags"></i> <?php the_category( ', ' ); ?></p>
+                        <p class="categories"><i class="fas fa-tags"></i> <?php the_category(', '); ?></p>
                     </div>
                 </div>
         <?php
@@ -39,65 +48,6 @@
  -->
 <!-- Display the Title as a link to the Post's permalink. -->
 
-
-
-
-
-<!-- at first we do all that in in static way then dyamic -->
-
-<!-- <div class="container">
-    <div class="row">
-        <div class="col-sm-6">
-            <div class="main-post">
-                <h3 class="post-title">This is the post title</h3>
-                <span class="post-author"><i class="fas fa-user"></i> Osama, </span>
-                <span class="post-date"> <i class="fas fa-calendar-alt"></i> 13/13/2013, </span>
-                <span class="post-comments"> <i class="fas fa-comments"></i> 32 Comment</span>
-                <img class="img-fluid img-thumbnail" src="https://dummyimage.com/500x300/000/fff/&text=Ezlero Theme Placeholder Image" alt="place holder image">
-                <span class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus possimus asperiores fuga dolorum. Rem laboriosam fuga eum et repudiandae similique, obcaecati aliquid harum enim ab voluptatum cupiditate consectetur perferendis qui.</span>
-                <hr>
-                <p class="categories"><i class="fas fa-tags"></i> HTML, Testing, Coding</p>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="main-post">
-                <h3 class="post-title">This is the post title</h3>
-                <span class="post-author"><i class="fas fa-user"></i> Osama, </span>
-                <span class="post-date"> <i class="fas fa-calendar-alt"></i> 13/13/2013, </span>
-                <span class="post-comments"> <i class="fas fa-comments"></i> 32 Comment</span>
-                <img class="img-fluid img-thumbnail" src="https://dummyimage.com/500x300/000/fff/&text=Ezlero Theme Placeholder Image" alt="place holder image">
-                <span class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus possimus asperiores fuga dolorum. Rem laboriosam fuga eum et repudiandae similique, obcaecati aliquid harum enim ab voluptatum cupiditate consectetur perferendis qui.</span>
-                <hr>
-                <p class="categories"><i class="fas fa-tags"></i> HTML, Testing, Coding</p>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="main-post">
-                <h3 class="post-title">This is the post title</h3>
-                <span class="post-author"><i class="fas fa-user"></i> Osama, </span>
-                <span class="post-date"> <i class="fas fa-calendar-alt"></i> 13/13/2013, </span>
-                <span class="post-comments"> <i class="fas fa-comments"></i> 32 Comment</span>
-                <img class="img-fluid img-thumbnail" src="https://dummyimage.com/500x300/000/fff/&text=Ezlero Theme Placeholder Image" alt="place holder image">
-                <span class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus possimus asperiores fuga dolorum. Rem laboriosam fuga eum et repudiandae similique, obcaecati aliquid harum enim ab voluptatum cupiditate consectetur perferendis qui.</span>
-                <hr>
-                <p class="categories"><i class="fas fa-tags"></i> HTML, Testing, Coding</p>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="main-post">
-                <h3 class="post-title">This is the post title</h3>
-                <span class="post-author"><i class="fas fa-user"></i> Osama, </span>
-                <span class="post-date"> <i class="fas fa-calendar-alt"></i> 13/13/2013, </span>
-                <span class="post-comments"> <i class="fas fa-comments"></i> 32 Comment</span>
-                <img class="img-fluid img-thumbnail" src="https://dummyimage.com/500x300/000/fff/&text=Ezlero Theme Placeholder Image" alt="place holder image">
-                <span class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus possimus asperiores fuga dolorum. Rem laboriosam fuga eum et repudiandae similique, obcaecati aliquid harum enim ab voluptatum cupiditate consectetur perferendis qui.</span>
-                <hr>
-                <p class="categories"><i class="fas fa-tags"></i> HTML, Testing, Coding</p>
-            </div>
-        </div>
-
-    </div>
-</div> -->
 
 <?php
 
