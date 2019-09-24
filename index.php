129ebb13a391e6eb0e365d014a886a3efd56bc7d
Watheq <watheq.alshowaiter@gmail.com>
2019-09-24 16:44:10 +0300

    vid #63 - adding pagination with some styling

--- index.php
+++ index.php
@@ -27,26 +27,53 @@
                         <!-- post imsge thunmbnail -->
                         <!-- 1st arg is for sizes like 'thumbnals' 150x150 and other sizes in worpreess docs -->
                         <?php the_post_thumbnail("", ["class" => "img-fluid img-thumbnail", "alt" => "place holder image", "title" => "post image"]); ?>
-                   
+
                         <div class="post-content" id="post-content">
-                            <?php // the_content('Continue.. '); ?>
-                            <?php  the_excerpt(); ?>
+                            <?php // the_content('Continue.. '); 
+                                    ?>
+                            <?php the_excerpt(); ?>
                             <a href="<?php the_permalink(); ?>" rel="bookmark" title="Complete Reading <?php the_title_attribute(); ?>">Read More...</a>
                         </div>
                         <!-- <span class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus possimus asperiores fuga dolorum. Rem laboriosam fuga eum et repudiandae similique, obcaecati aliquid harum enim ab voluptatum cupiditate consectetur perferendis qui.</span> -->
                         <hr>
-                        <p class="categories"><i class="fas fa-tags"></i> <?php the_category(', '); ?></p>
+                        <p class="post-categories"><i class="fas fa-tags"></i> <?php the_category(', '); ?></p>
+                        <p clas="post-tags">
+                            <?php
+                                    if (has_tag()) {
+                                        the_tags();
+                                    } else {
+                                        echo "<i>Thers no tags</i>";
+                                    }
+                                    ?>
+                        </p>
                     </div>
                 </div>
         <?php
             } // end while
-        } // end if
+        } // end if 
         ?>
-    </div>
+          
+
+
+    </div> <!-- end .row ---> 
+    <div class="post-pagination">
+                <?php
+                // make some text to know it is prevous/next pages 
+                if (get_previous_posts_link()) {
+                    previous_posts_link('<i class="fas fa-chevron-left fa-fw fa-lg"></i> Prev');
+                } else {
+                    echo "<span class='prev-span'>Prev</span>";
+                }
+
+                if (get_next_posts_link()) {
+                    next_posts_link('Next <i class="fas fa-chevron-right fa-fw fa-lg"></i>');
+                } else {
+                    echo "<span class='next-span'>Next</span>";
+                }
+                ?>
+            </div>
 </div>
-
-
-<!-- 
+<!--  
     this is the post loop 
  -->
 <!-- Display the Title as a link to the Post's permalink. -->
